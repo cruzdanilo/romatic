775d820b721b3b1c567beadf0c122791458a9ba7
Danilo Neves Cruz <cruzdanilo@gmail.com>
2017-07-25 16:20:39 -0300

    Generate result zips

--- romatic.js
+++ romatic.js
@@ -7,6 +7,33 @@ const xml2js = require('xml2js');
 const xpath = require('xml2js-xpath');
 const manifest = require('./package.json');
 
+async function processROM(rom, dat) {
+  // eslint-disable-next-line
+  const crc = (rom._data.crc32 >>> 0).toString(16).toUpperCase().padStart(8, '0');
+  // eslint-disable-next-line
+  const size = rom._data.uncompressedSize;
+  const info = xpath.evalFirst(dat, `//rom[@crc='${crc}']`);
+  if (!info || parseInt(info.$.size, 10) !== size) {
+    console.log(`UNKNOWN: "${rom.name}" ${crc}`);
+    return null;
+  }
+  if (info.found) {
+    console.log(`DUPLICATE: ${info.$.name}`);
+    return null;
+  }
+  info.found = true;
+  if (rom.name !== info.$.name) {
+    console.log(`WRONG NAME: ${rom.name} != ${info.$.name}`);
+  }
+  const basename = path.basename(info.$.name, path.extname(info.$.name));
+  return rom.async('nodebuffer').then(data =>
+    new JSZip().file(info.$.name, data).generateAsync({
+      type: 'nodebuffer',
+      compression: 'DEFLATE',
+      compressionOptions: { level: 9 },
+    }).then(res => fs.writeFile(path.resolve('out', `${basename}.zip`), res)));
+}
+
 const parser = new ArgumentParser({
   version: manifest.version,
   description: manifest.description,
@@ -14,24 +41,16 @@ const parser = new ArgumentParser({
 parser.addArgument('dat', { help: 'Path to a DAT file.' });
 parser.addArgument('roms', { help: 'Path to ROM directory.' });
 const args = parser.parseArgs();
-const xml = fs.readFileSync(args.dat);
-xml2js.parseString(xml, async (errDat, dat) => {
+xml2js.parseString(fs.readFileSync(args.dat), async (errDat, dat) => {
   if (errDat) throw errDat;
-  await fs.readdir(args.roms).then(roms => Promise.all(roms.map(async (f) => {
-    const zip = await fs.readFile(path.resolve(args.roms, f)).then(z => JSZip.loadAsync(z));
-    Object.values(zip.files).forEach((rom) => {
-      // eslint-disable-next-line
-      const crc = (rom._data.crc32 >>> 0).toString(16).toUpperCase().padStart(8, '0');
-      // eslint-disable-next-line
-      const size = rom._data.uncompressedSize;
-      const romData = xpath.evalFirst(dat, `//rom[@crc='${crc}']`);
-      if (!romData) { console.warn(`UNKNOWN: "${f}/${rom.name}" ${crc}`); return; }
-      if (parseInt(romData.$.size, 10) !== size) console.warn(`WRONG SIZE: "${f}/${rom.name}" ${size}`);
-      if (romData.found) console.warn('DUPLICATE:', rom.name);
-      romData.found = true;
-    });
-  })));
+  await fs.readdir(args.roms).then(roms =>
+    roms.reduce((promise, file) =>
+      promise.then(() =>
+        fs.readFile(path.resolve(args.roms, file)).then(f =>
+          JSZip.loadAsync(f).then(z =>
+            Promise.all(Object.values(z.files).map(rom =>
+              processROM(rom, dat)))))), Promise.resolve()));
   xpath.find(dat, '//rom').filter(r => !r.found).forEach((rom) => {
-    console.warn('MISSING:', rom.$.name);
+    console.log('MISSING:', rom.$.name);
   });
 });
